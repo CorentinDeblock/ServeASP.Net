570c28864b0f0dfe9e5dba7a8778f88691b3b49a
c.deblock <c.deblock@FORMA1304>
2021-07-23 15:50:54 +0200

    Ajut des modification

---
+++ template/IntServices.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceASP.Bases;
+using ServiceASP.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceASP.template
+{
+    public interface IIntService<Model, Form> : IServices<Model, Form, int> { }
+    public abstract class IntServices<DataContext, Entity, Model, Form> :
+        BaseServices<DataContext, Entity, Model, Form, int>,
+        IIntService<Model, Form>
+        where Entity : class, IModel<int>
+        where DataContext : DbContext
+    {
+        protected IntServices(DataContext dc, IMapper<Entity, Model, Form> mapper) : base(dc, mapper)
+        {
+        }
+    }
+}
